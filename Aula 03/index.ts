f07ee708b57b5ee5c7d6f0d1769e6b7d1bde98ab
Vitor Padilha <vitorpadilhagoncalves@gmail.com>
2024-10-25 20:09:21 -0300

    Aula 3

--- Aula 03/index.ts
+++ Aula 03/index.ts
@@ -16,9 +16,9 @@ function soma(x: number, y: number): number {
 fazOperacao(soma);
 
 //Usando Spread (...)
-var a = {"nome": "Vitor", "cidade": "Nova Friburgo"};
+var a1 = {"nome": "Vitor", "cidade": "Nova Friburgo"};
 var b = {"cidade": "Miracema", "gostosMusicais": ["Rock"]};
-var c = {...a, ...b};
+var c = {...a1, ...b};
 console.log(c);
 var d  = [1,2];
 var e = [3, 4];
